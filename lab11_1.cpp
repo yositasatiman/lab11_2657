70287335c7585b27a4677e18606fa913c72176a3
Yosita satiman <yosita_satiman@cmu.ac.th>
2024-12-20 14:36:56 +0700

    lab11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,20 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main (){
+
+    srand(time(0)) ;
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";  
+    cin.get();
+    cin.get();
+    cin.get();
+    int num = rand() % 9;
+    cout << "You will get "<< grade[num]<<" in this 261102."; 
+
+    return 0;
+}
